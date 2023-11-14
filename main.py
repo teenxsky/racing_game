c0f556da5f68334cfafd620c3393ba241de73c3f
Roman Sokolovskii <romansokolv209@gmail.com>
2023-11-15 07:44:15 +1000

    #button can change color

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ clock = pg.time.Clock()
 
 action = True
 
+start_button_color = (228, 50, 35)
 start_button = pygame.Surface((250, 100), 10)
-start_button.fill((250, 60, 85))
+
 start_button_rect = start_button.get_rect(center=(W // 2, H // 2))
 
 
@@ -24,7 +25,8 @@ other_font = pygame.font.Font("fonts/pixfont.ttf", 80)
 title_text = title_font.render("00 RACING", False, (255, 255, 255))
 title_text_rect = title_text.get_rect(center=(W // 2, H // 6))
 
-start_text = other_font.render("START", False, (255, 255, 255))
+start_text_color = (255, 255, 255)
+start_text = other_font.render("START", False, start_text_color)
 start_text_rect = start_text.get_rect(center=(W // 2, H // 2))
 
 background = pygame.image.load("images/background2.png")
@@ -58,6 +60,17 @@ while action:
             if event.key == pygame.K_w:
                 motor_sound.play()
                 #skype_sound.play()
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    in_button_x = W // 2 - start_button.get_width() // 2 <= mouse_x <=W // 2 + start_button.get_width() // 2
+    in_button_y = H // 2 - start_button.get_height() // 2 <= mouse_y <= W // 2 + start_button.get_height() // 2
+
+    if in_button_x and in_button_y:
+        start_button_color = (100, 100, 100)
+        start_button.fill(start_button_color)
+    else:
+        start_button_color = (228, 50, 35)
+        start_button.fill(start_button_color)
 
     background_x -= 2
     if background_x == - 1280:
