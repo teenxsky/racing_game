90eeb240606825ac5ef5b2fbeca63450eb292654
Roman Sokolovskii <romansokolv209@gmail.com>
2023-11-18 20:45:52 +1000

    #add quit from gameloop

--- game.py
+++ game.py
@@ -2,27 +2,72 @@ import pygame as pg
 from menu import MainMenu
 from objects import Button, Picture
 
+pg.init()
+
 
 class Game:
     def __init__(self):
         pg.init()
         self.running, self.playing = True, False
         self.SCREEN_WIDTH, self.SCREEN_HEIGHT = 1280, 720
+        self.clicked = False
         self.screen = pg.Surface((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         self.window = pg.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-        self.FPS = 30
+        self.FPS = 60
         self.font_name = "fonts/pixfont.ttf"
         self.frame_per_second = pg.time.Clock()
 
         self.main_menu = MainMenu(self)
+        button_sound = pg.mixer.Sound("audio/button_sound.mp3")
+
+        start_but_off = pg.image.load("images/buttons/start_button_off.png").convert_alpha()
+        start_but_on = pg.image.load("images/buttons/start_button_on.png").convert_alpha()
+        self.start_button = Button(100, 70, start_but_off, start_but_on, button_sound, 0.3)
+
+        garage_but_off = pg.image.load("images/buttons/garage_button_off.png").convert_alpha()
+        garage_but_on = pg.image.load("images/buttons/garage_button_on.png").convert_alpha()
+        self.garage_button = Button(100, 190, garage_but_off, garage_but_on, button_sound, 0.3)
+
+        music_but_off = pg.image.load("images/buttons/music_button_off.png").convert_alpha()
+        music_but_on = pg.image.load("images/buttons/music_button_on.png").convert_alpha()
+        self.music_button = Button(100, 310, music_but_off, music_but_on, button_sound, 0.3)
+
+        sets_but_off = pg.image.load("images/buttons/settings_button_off.png").convert_alpha()
+        sets_but_on = pg.image.load("images/buttons/settings_button_on.png").convert_alpha()
+        self.sets_button = Button(100, 430, sets_but_off, sets_but_on, button_sound, 0.3)
+
+        quit_but_off = pg.image.load("images/buttons/quit_button_off.png").convert_alpha()
+        quit_but_on = pg.image.load("images/buttons/quit_button_on.png").convert_alpha()
+        self.quit_button = Button(100, 550, quit_but_off, quit_but_on, button_sound, 0.3)
+
+        title_image = pg.image.load("images/title_name.png")
+        self.title_picture = Picture(400, 15, title_image, 0.5)
+
+        close_but_off = pg.image.load("images/buttons/close_button_off.png").convert_alpha()
+        close_but_on = pg.image.load("images/buttons/close_button_on.png").convert_alpha()
+        self.close_button = Button(550, 300, close_but_off, close_but_on, button_sound, 0.25)
+
+        back_but_off = pg.image.load("images/buttons/back_button_off.png").convert_alpha()
+        back_but_on = pg.image.load("images/buttons/back_button_on.png").convert_alpha()
+        self.back_button = Button(670, 300, back_but_off, back_but_on, button_sound, 0.25)
+
+        self.game_state = "GAME"
 
     def game_loop(self):
         pg.display.set_caption("gameloop")
+        self.game_state = "GAME"
+
         while self.playing:
             self.check_events()
 
             self.screen.fill('black')
             self.window.blit(self.screen, (0, 0))
+
+            if self.game_state == "PAUSE":
+                if self.close_button.draw(self.window):
+                    self.playing = False
+                if self.back_button.draw(self.window):
+                    self.game_state = "GAME"
 
             pg.display.update()
             self.frame_per_second.tick(self.FPS)
@@ -33,6 +78,12 @@ class Game:
             if event.type == pg.QUIT:
                 self.running, self.playing = False, False
                 self.main_menu.run_display = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    if self.game_state == "PAUSE":
+                        self.game_state = "GAME"
+                    else:
+                        self.game_state = "PAUSE"
 
     def reset_keys(self):
         self.screen = pg.Surface((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ from objects import Button, Picture
 
 class Menu:
     def __init__(self, game):
+        pg.init()
         self.game = game
         self.run_display = True
 
@@ -19,51 +20,32 @@ class MainMenu(Menu):
         Menu.__init__(self, game)
         self.state = None
 
-    def display_menu(self):
         button_sound = pg.mixer.Sound("audio/button_sound.mp3")
 
-        start_but_off = pg.image.load("images/buttons/start_button_off.png").convert_alpha()
-        start_but_on = pg.image.load("images/buttons/start_button_on.png").convert_alpha()
-        start_button = Button(100, 70, start_but_off, start_but_on, button_sound, 0.3)
 
-        garage_but_off = pg.image.load("images/buttons/garage_button_off.png").convert_alpha()
-        garage_but_on = pg.image.load("images/buttons/garage_button_on.png").convert_alpha()
-        garage_button = Button(100, 190, garage_but_off, garage_but_on, button_sound, 0.3)
 
-        music_but_off = pg.image.load("images/buttons/music_button_off.png").convert_alpha()
-        music_but_on = pg.image.load("images/buttons/music_button_on.png").convert_alpha()
-        music_button = Button(100, 310, music_but_off, music_but_on, button_sound, 0.3)
-
-        sets_but_off = pg.image.load("images/buttons/settings_button_off.png").convert_alpha()
-        sets_but_on = pg.image.load("images/buttons/settings_button_on.png").convert_alpha()
-        sets_button = Button(100, 430, sets_but_off, sets_but_on, button_sound, 0.3)
-
-        quit_but_off = pg.image.load("images/buttons/quit_button_off.png").convert_alpha()
-        quit_but_on = pg.image.load("images/buttons/quit_button_on.png").convert_alpha()
-        quit_button = Button(100, 550, quit_but_off, quit_but_on, button_sound, 0.3)
-
-        title_image = pg.image.load("images/title_name.png")
-        title_picture = Picture(400, 15, title_image, 0.5)
+    def display_menu(self):
 
         self.run_display = True
         pg.display.set_caption("menu")
+
         while self.run_display:
 
             self.game.screen.fill('white')
             self.game.check_events()
             self.check_input()
 
-            title_picture.draw_with_pulse(self.game.screen, 15)
+            self.game.title_picture.draw_with_pulse(self.game.screen, 15)
 
-            if start_button.draw(self.game.screen):
+            if self.game.start_button.draw(self.game.screen):
                 self.state = "START"
-            if garage_button.draw(self.game.screen):
+            if self.game.garage_button.draw(self.game.screen):
                 self.state = "GARAGE"
-            if music_button.draw(self.game.screen):
+            if self.game.music_button.draw(self.game.screen):
                 self.state = "MUSIC"
-            if sets_button.draw(self.game.screen):
+            if self.game.sets_button.draw(self.game.screen):
                 self.state = "SETS"
-            if quit_button.draw(self.game.screen):
+            if self.game.quit_button.draw(self.game.screen):
                 self.game.running, self.game.playing, self.run_display = False, False, False
 
             self.blit_screen()
@@ -78,6 +60,7 @@ class MainMenu(Menu):
             pass
         elif self.state == "SETS":
             pass
+        self.state = None
 
 '''
 class SetsMenu(Menu):
